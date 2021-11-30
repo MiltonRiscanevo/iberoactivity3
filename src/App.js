3c2f76fa2235721ed34b8ccd9b776da6d52bc54c
Milton Riscanevo <miltonriscanevo@hotmail.com>
2021-11-30 08:39:20 -0500

    adicionando tarea ibero

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import diagram from './images/Diagrama de contexto.jpg';
+import Notas from './images/Notas del diagrama.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Actividad 3 Limites de contexto</h1>
+        <img src={diagram} className="App-logo" alt="logo" />
+        <img src={Notas} className="App-logo" alt="logo" />
+        <p>presentado a: Karla Yohana Sanchez <br/>Presentado por: Milton Riscanevo</p>
+    </div>
+  );
+}
+
+export default App;
